357c5bc49b1df8c1aa3b2d3f45a636102b6349ae
unknown <samanmhmd73@gmail.com>
2022-06-30 03:22:49 +0430

    install and use yaml package and edit yaml file

--- config/app.php
+++ config/app.php
@@ -210,6 +210,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Yaml' => 'Symfony\Component\Yaml\Yaml',
+
     ])->toArray(),
 
 ];
